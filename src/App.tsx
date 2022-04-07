2bd8782fa7c9592f9e5426daafd5e46c777566a3
Tim La <tlamarketing111@gmail.com>
2022-04-07 15:27:58 -0400

    created intro component to learn basics of typescript

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1>App</h1>
+      <Intro />
       <Intro />
     </div>
   )
--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
 
 const Intro = () => {
+
+    let name: string = "Tim";
+    name = "Bob";
+    name = 7;
+
     return (
-        <h2>Intro</h2>
+        <div style={{ border: "1px solid black", width: "50vw" }}>
+            <h1>Intro Component</h1>
+            <h3>Hi, {name}</h3>
+            <p>Name is displayed with reassigned value, but error is shown in code</p>
+        </div>
+
     )
 }
 
-export default Intro
+export default Intro;
